9a15d5d52047dcc247405f1096c4be357a8d87b0
Bill S <will.staf@gmail.com>
2019-03-09 16:59:04 +0000

    working on it

--- midiOut.py
+++ midiOut.py
@@ -14,9 +14,11 @@ def sigHandler(signo, frame):
 atexit.register(onExit)
 signal.signal(signal.SIGTERM, sigHandler)
 
-midiOut = mido.open_input('Virtual Port', virtual=True)
+midiOut = mido.open_output('Virtual Port', virtual=True)
 while True:
     #midiOut.sen
     #print ("tick")
-    time.sleep(0.01)
+    time.sleep(1.0)
+    msg = mido.Message('note_on', note=60)
+    midiOut.send(msg)
 print ("Last Line..........")
