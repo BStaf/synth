a1ed0e47ea3e204773e1fa4801d7a2cd96eab047
Bill S <will.staf@gmail.com>
2019-04-12 03:31:50 +0100

    keyboard is functional

--- midiOut.py
+++ midiOut.py
@@ -4,37 +4,68 @@ import signal
 import mido
 import time
 import logging
+import RPi.GPIO as GPIO
 from midiInFS import midiInFS
 
 SoundFontFile = "/home/pi/soundfonts/FluidR3_GM.sf2"
 
+PIN_DATA = 14
+PIN_LATCH = 15
+PIN_CLOCK = 18
+
+KeyboardDict = {}
+#KeyboardRow = [20,16,12,7,8,25,24,23]
+KeyboardRow = [23,24,25,8,7,12,16,20]
+
 def onExit():
-    logging.debug('Exiting')
+    logging.info('Exiting')
     midiOut.close()
 
 def sigHandler(signo, frame):
     sys.exit(0)
 
+def shiftOut(byte):
+    GPIO.output(PIN_LATCH, 0)
+    for x in range(8):
+        GPIO.output(PIN_DATA, (byte >> x) & 1)
+        GPIO.output(PIN_CLOCK, 1)
+        GPIO.output(PIN_CLOCK, 0)
+    GPIO.output(PIN_LATCH, 1)
+
 def changeInstrument(inst):
     msg = mido.Message('program_change', program =inst)
     midiOut.send(msg)
 
-def playNote(midiOut, note, hold):
-    msg = mido.Message('note_on', note=note)
-    midiOut.send(msg)
-    time.sleep(hold)
-    msg = mido.Message('note_off', note=note)
-    midiOut.send(msg)
- 
+def playNote(note, on):
+    if on:
+        msg = mido.Message('note_on', note=note)
+        midiOut.send(msg)
+       # print (note," is on" )
+    else:
+        msg = mido.Message('note_off', note=note)
+        midiOut.send(msg)
+        #print (note," is off" )
+
+def handleKeyPress(channel):
+    if channel == In1:
+        playNote(60, not GPIO.input(channel))
+    if channel == In2:
+        playNote(62, not GPIO.input(channel))
+    if channel == In3:
+        playNote(64, not GPIO.input(channel))
+    if channel == In4:
+        playNote(65, not GPIO.input(channel))
+
 atexit.register(onExit)
 signal.signal(signal.SIGTERM, sigHandler)
 
+logging.info('Starting FluidSynth')
 midiIn = midiInFS()
-#midiIn.testFunc()
 midiIn.stopFluidSynth()
 time.sleep(1)
 midiIn.initFluidSynth(SoundFontFile)
-print("waiting for FluidSynth port")
+
+logging.info('Connecting to FluidSynth port')
 fsPort = []
 timeout = time.time() + 30
 while time.time() < timeout:
@@ -47,23 +78,34 @@ while time.time() < timeout:
 else:
     logging.error("Failed to link to FluidSynth")
 
+global midiOut
 midiOut = mido.open_output(fsPort[0])
-print (fsPort)
+
+changeInstrument(30)
+print ("Connected")
 
 incInst = 0
-hold = 0.15
+baseMidiNote = 48
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+for i in range(8):
+    GPIO.setup(KeyboardRow[i], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+GPIO.setup(PIN_DATA, GPIO.OUT)
+GPIO.setup(PIN_LATCH, GPIO.OUT)
+GPIO.setup(PIN_CLOCK, GPIO.OUT)
 
 while True:
-    if incInst > 127:
-        incInst = 0
-    changeInstrument(incInst)
-    playNote(midiOut, 60, hold)
-    playNote(midiOut, 62, hold)
-    playNote(midiOut, 64, hold)
-    playNote(midiOut, 62, hold)
-    playNote(midiOut, 65, hold)
-    playNote(midiOut, 67, hold)
-    playNote(midiOut, 65, hold)
-    playNote(midiOut, 69, hold)
-    incInst += 1
+    for i in range(8):
+        shiftOut(1 << i)
+        for j in range(8):
+            note = baseMidiNote+(8*i)+j
+            if not note in KeyboardDict:
+                KeyboardDict[note] = 2
+            if GPIO.input(KeyboardRow[j]) == 1 and KeyboardDict[note] != 1:
+                playNote(note, 1)
+                KeyboardDict[note] = 1
+            elif GPIO.input(KeyboardRow[j]) == 0 and KeyboardDict[note] == 1:
+                playNote(note, 0)
+                KeyboardDict[note] = 0
 print ("Last Line..........")
